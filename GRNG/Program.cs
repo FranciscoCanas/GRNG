4f888eced403987ba084686b1066dc31699a4f7b
Francisco Canas <mailfrancisco@gmail.com>
2012-07-29 13:28:10 -0400

    Adding to repo

---
+++ GRNG/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GRNG
+{
+    class Program
+    {
+        /** 
+         * Just a short test.
+         **/
+        static public void Main()
+        {
+            int MEAN = 50;
+            int VARIANCE = 100;
+            int TEST_SIZE = 1000;
+            double total=0;
+            double[] sample = new double[TEST_SIZE];
+            
+            GRandom rand = new GRandom();
+
+            for (int i = 0; i < TEST_SIZE; i++)
+            {
+                sample[i] = rand.GetNormalDouble(MEAN, VARIANCE);
+                Console.WriteLine(sample[i]);
+                total += sample[i];
+            }
+
+            Console.WriteLine("Total: " + (total));
+            Console.WriteLine("Mean: " + GRandom.Mean(sample, TEST_SIZE));
+            Console.WriteLine("Variance: " + GRandom.variance(sample, TEST_SIZE));
+        }
+    }
+}
